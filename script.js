6d2e0eff502f6172ac1da7ee1128516e7ab1f6b0
briargg <159084445+briargg@users.noreply.github.com>
2024-10-22 17:30:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const signUpButton = document.querySelector('button');
-  
-    signUpButton.addEventListener('click', (e) => {
-      e.preventDefault();
-      signUpButton.classList.add('shake');
-  
-      setTimeout(() => {
-        signUpButton.classList.remove('shake');
-      }, 500);
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtén todos los enlaces del menú lateral
+    const menuLinks = document.querySelectorAll('.menu-link');
+    // Recorre cada enlace del menú
+    menuLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            // Oculta todas las secciones
+            document.querySelectorAll('.content-section').forEach(section => {
+                section.classList.remove('active');
+            });
+            // Muestra la sección correspondiente
+            const sectionId = `${this.getAttribute('data-section')}-content`;
+            document.getElementById(sectionId).classList.add('active');
+        });
     });
-  });
-  
+});
